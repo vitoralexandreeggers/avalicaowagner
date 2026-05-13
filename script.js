dd0d36d861d120b93a136d67f3cae0afa9a747cd
vitoralexandreeggers <eggers.vitor@escola.pr.gov.br>
2026-05-13 12:24:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-function giovanalinda() {
-    fetch('dados.json')
-        .then(response => response.json())
-        .then(dadinhos =>{
-            tablet = document.getElementById('giovana');
-
-            dadinhos.forEach(pessoinha => {
-                let linha = document.createElement('tr');
-
-                linha.innerHTML =
-                "<td>" + pessoinha.nome + "</td>" + 
-                "<td>" + pessoinha.idade + "</td>" +
-                "<td>" + pessoinha.cor + "</td>" +
-                "<td>" + pessoinha.time + "</td>" +
-                "<button>" + pessoinha.time + "</td>" +
-                tablet.appendChild(linha);
-            });
-        }) 
-}
-
 async function giovanalinda() {
     const destino = document.getElementById('giovana');
     if (!destino) return;
